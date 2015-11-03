8311c5c75db5a087753ee125bdab4062862c32a2
Kevin van Hulst <kevin.van.hulst@gmail.com>
2015-11-03 10:48:49 +0100

    Added test data and a testData view

---
+++ SuzieQLisa.Web/src/SuzieQLisa/wwwroot/Database.js
@@ -0,0 +1,12 @@
+﻿export class Database {
+    getData(){
+        return [
+            {"id": 1, "name": "Kevin van Hulst", "class": "LPICO15L1", "studentNumber": 99016948},
+            {"id": 2, "name": "Rik Pronk", "class": "LPICO15L1", "studentNumber": 99016312},
+            {"id": 3, "name": "Pietje van Genderen", "class": "LPICO15L1", "studentNumber": 99045674},
+            {"id": 4, "name": "Jan-Willen Willemszoon", "class": "LPICO12A3", "studentNumber": 99012345},
+            {"id": 5, "name": "Willie-Alex van Oranje", "class": "LPICO14L1", "studentNumber": 99034561},
+            {"id": 6, "name": "Bjørn Bjørnsson", "class": "LPICO12A3", "studentNumber": 99079864}
+        ];
+    }
+}
--- SuzieQLisa.Web/src/SuzieQLisa/wwwroot/app.js
+++ SuzieQLisa.Web/src/SuzieQLisa/wwwroot/app.js
@@ -1,11 +1,11 @@
 ﻿export class App {
     constructor() {
-        this.message = "Hello world!";
+        this.message = "Hello world!";  
     }
 
     configureRouter(config, router) {
         config.map([
-
+            { route: "testData", name: "testData", moduleId: "testData", nav: true}
         ])
     }
 }
