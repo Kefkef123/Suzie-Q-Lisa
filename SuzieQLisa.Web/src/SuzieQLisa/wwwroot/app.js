8acd887ee7ebfd8dcee0f060953ace0ad765a381
Rik Pronk <rikpronkie@gmail.com>
2015-11-03 12:57:00 +0100

    Select * From query can now be done and returns all values from the table

--- SuzieQLisa.Web/src/SuzieQLisa/wwwroot/app.js
+++ SuzieQLisa.Web/src/SuzieQLisa/wwwroot/app.js
@@ -5,7 +5,7 @@
 
     configureRouter(config, router) {
         config.map([
-            { route: "sql", name: "sql", moduleId: "sql", nav: true}
+            { route: "sql", name: "sql", moduleId: "sql", nav: true},
             { route: "testData", name: "testData", moduleId: "testData", nav: true}
         ])
     }
--- SuzieQLisa.Web/src/SuzieQLisa/wwwroot/sql.js
+++ SuzieQLisa.Web/src/SuzieQLisa/wwwroot/sql.js
@@ -1,43 +1,11 @@
-﻿export class Sql {
-    constructor() {
-        this.query = "";
+﻿import {SqlReader} from 'sqlreader';
+
+export class Sql {
+    static inject() {
+        return [ SqlReader ];
     }
 
-    readSql() {
-        this.query = $('section#editor textarea').val();
-
-        switch(this.query.substring(0, 6).toLowerCase()) {
-            case "select":
-                try {
-                    this.analyzeSelect(this.query);
-                }
-                catch (exception) {
-                    console.log(exception.message)
-                }
-                break;
-            case "insert":
-
-                break;
-            case "update":
-
-                break;
-            case "delete":
-
-                break;
-        }
-    }
-
-    analyzeSelect(query) {
-        var queryComponents = query.split(' ');
-        if (this.query.substring(0, 6).toLowerCase() !== "select")
-        {
-            throw {message: "Not a select query"}
-        }
-        if (queryComponents[2].toLowerCase() !== 'from')
-        {
-            throw {message: "Missing table selector"}
-        }
-
-        console.log("success");
+    constructor(sqlreader) {
+        this.sqlreader = sqlreader;
     }
 }
